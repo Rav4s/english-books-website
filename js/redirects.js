2f45c4811349e120583c122128cb5baf2a7961e2
rav4s <ravidhruvishshah@gmail.com>
2021-03-07 17:39:15 -0600

    Added more books, articles, and poems

--- js/redirects.js
+++ js/redirects.js
@@ -5,3 +5,18 @@ document.getElementById("go-down").onclick = function() {
 document.getElementById("centosdead").onclick = function() {
     location.href = "./centos-death.html";
 };
+document.getElementById("worm").onclick = function() {
+    location.href = "./worm.html";
+};
+document.getElementById("redmoonrising").onclick = function() {
+    location.href = "./redmoonrising.html";
+};
+document.getElementById("sonnetbillycollins").onclick = function() {
+    location.href = "./sonnetbillycollins.html";
+};
+document.getElementById("sonnetxii").onclick = function() {
+    location.href = "./sonnetxii.html";
+};
+document.getElementById("imacg3stevejobs").onclick = function() {
+    location.href = "./imacg3stevejobs.html";
+};
